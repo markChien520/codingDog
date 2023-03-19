4c10fcf0a7f0ae55d28bb7d6d1243d993d3c4c71
mark520710 <y15001110170@outlook.com>
2023-03-19 23:41:36 +0800

    add blank line

--- src/main/java/com/imooc/demo/config/Application.java
+++ src/main/java/com/imooc/demo/config/Application.java
@@ -20,6 +20,7 @@ public class Application {
 
         Environment env = app.run(args).getEnvironment();
 
+
         System.out.println("启动成功！！");
         System.out.println("根地址: \t\thttp://127.0.0.1:" + env.getProperty("server.port"));
         System.out.println("登录接口: \thttp://127.0.0.1:" + env.getProperty("server.port") +
