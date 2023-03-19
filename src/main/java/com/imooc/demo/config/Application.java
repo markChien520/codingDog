db066156da932b5fad5f115401daee6cdc3874cd
mark520710 <y15001110170@outlook.com>
2023-03-19 23:09:10 +0800

    init

---
+++ src/main/java/com/imooc/demo/config/Application.java
@@ -0,0 +1,28 @@
+package com.imooc.demo.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com") //扫描
+@MapperScan("com.imooc.demo.mapper")//mabatis 映射类
+public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        Environment env = app.run(args).getEnvironment();
+
+        System.out.println("启动成功！！");
+        System.out.println("根地址: \t\thttp://127.0.0.1:" + env.getProperty("server.port"));
+        System.out.println("登录接口: \thttp://127.0.0.1:" + env.getProperty("server.port") +
+                "/user/login?loginName=test&password=test");
+    }
+
+}
