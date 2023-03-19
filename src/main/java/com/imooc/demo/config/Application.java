f73e9e36b98e7f4d80458ca0fea7dddec4158fcc
mark520710 <y15001110170@outlook.com>
2023-03-19 23:24:33 +0800

    ignore

--- src/main/java/com/imooc/demo/config/Application.java
+++ src/main/java/com/imooc/demo/config/Application.java
@@ -17,6 +17,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
+
         Environment env = app.run(args).getEnvironment();
 
         System.out.println("启动成功！！");
